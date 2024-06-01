69922c16c9f02c29fc7e7aa0b42ce2730614b319
TARUN GUPTA <tarun.ey@tataaig.com>
2024-06-01 16:05:53 +0530

    App Name changed

--- ios/rntypescriptBoilerplateTests/rntypescriptBoilerplateTests.m
+++ ios/rntypescriptBoilerplateTests/rntypescriptBoilerplateTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RNTypescriptBoilerplateTests : XCTestCase
+@interface AstroBuddyTests : XCTestCase
 
 @end
 
-@implementation RNTypescriptBoilerplateTests
+@implementation AstroBuddyTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
